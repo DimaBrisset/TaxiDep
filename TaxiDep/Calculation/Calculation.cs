ac220d01323137900298b4d9da16de365bcd6d31
Dima <DimaBrisset@gmail.com>
2022-07-01 12:44:26 +0300

    add menu

--- TaxiDep/Calculation/Calculation.cs
+++ TaxiDep/Calculation/Calculation.cs
@@ -8,11 +8,134 @@ namespace TaxiDep
 {
     internal class Calculation
     {
-     
+
+        private readonly List<IProduction> _allCars;
 
 
+        public Calculation(List<IProduction> cars)
+        {
+
+            _allCars = cars ?? new List<IProduction>();
+
+
+        }
+
+
+        public int GetCarsCount()
+        {
+            return _allCars.Count;
+
+        }
+
+        public void WithOutSortPrice()
+        {
+            Console.WriteLine($"We have {GetCarsCount()} cars");
+
+
+            for (int i = 0; i < _allCars.Count; i++)
+            {
+                Console.WriteLine(_allCars[i].ToString());
+            }
+        }
+
+
+
+        public void SortPriceByDescending()
+        {
+
+
+            _allCars.Sort((x, y) => x.GetPrice().CompareTo(y.GetPrice()));
+            _allCars.Reverse();
+            for (int i = 0; i < _allCars.Count; i++)
+            {
+                Console.WriteLine(_allCars[i].ToString());
+            }
+        }
+        public void SortPriceByAscending()
+        {
+
+
+            _allCars.Sort((x, y) => x.GetPrice().CompareTo(y.GetPrice()));
+            _allCars.Reverse();
+            _allCars.Reverse();
+            for (int i = 0; i < _allCars.Count; i++)
+            {
+                Console.WriteLine(_allCars[i].ToString());
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public void SortFuelByAscending()
+        {
+
+
+            Car[] carsCopy = new Car[_allCars.Count];
+            _allCars.Sort((x, y) => x.GetFuelConsumption().CompareTo(y.GetFuelConsumption()));
+            _allCars.Reverse();
+            _allCars.Reverse();
+            for (int i = 0; i < carsCopy.Length; i++)
+            {
+                Console.WriteLine(_allCars[i].ToString());
+            }
+        }
+
+
+        public void SortFuelByDescending()
+        {
+
+            _allCars.Sort((x, y) => x.GetFuelConsumption().CompareTo(y.GetFuelConsumption()));
+            _allCars.Reverse();
+            for (int i = 0; i < _allCars.Count; i++)
+            {
+                Console.WriteLine(_allCars[i].ToString());
+            }
+        }
+
+
+        public void PriceCar()
+        {
+
+            int sum = 0;
+            for (int i = 0; i < _allCars.Count; i++)
+            {
+                sum = sum + _allCars[i].GetPrice();
+
+            }
+            Console.WriteLine(sum);
+        }
+
+
+        public void SearchCarsSpeed(int minPrice, int maxPrice)
+        {
+
+            Console.WriteLine("Search Auto speed");
+            foreach (var car in _allCars)
+            {
+
+                if (car.GetSpeed() >= minPrice && car.GetSpeed() <= maxPrice)
+                {
+                    Console.WriteLine(car);
+                }
+
+            }
+
+        }
 
 
 
     }
+
+
+
+
 }
+
--- TaxiDep/Program.cs
+++ TaxiDep/Program.cs
@@ -14,20 +14,79 @@ namespace TaxiDep
     {
         static void Main(string[] args)
         {
-            IFactory creator = new GasFactory();  
+            IFactory creator = new GasFactory();
             IProduction car1 = creator.Create();
             IProduction car2 = creator.Create();
-           
-            creator =new TrackFactory();
-            IProduction truck1=creator.Create();
+
+            creator = new TrackFactory();
+            IProduction truck1 = creator.Create();
             IProduction truck2 = creator.Create();
 
-            creator =new ElectroFactory();
-            IProduction electro1=creator.Create();
+            creator = new ElectroFactory();
+            IProduction electro1 = creator.Create();
             IProduction electro2 = creator.Create();
 
-            List<IProduction> cars = new List<IProduction>() {  car1,truck1,electro1,};
-            Test carservice = new Test(cars);
+            List<IProduction> cars = new List<IProduction>() {car1,car2, truck1, truck2, electro1,electro2};
+            Calculation carservice = new Calculation(cars);
+
+
+            bool exit = false;
+            do
+            {
+
+                Console.WriteLine("Button # 1, Show all AUTO");
+                Console.WriteLine("Button # 2, Сost of all cars");
+                Console.WriteLine("Button # 3, Sort Fuel By Ascending");
+                Console.WriteLine("Button # 4, Sort Fuel By Descending");
+                Console.WriteLine("Button # 5, Sort Price By Ascending");
+                Console.WriteLine("Button # 6, Sort Price By Descending");
+                Console.WriteLine("Button # 7, Search Cars Speed");
+                Console.WriteLine("Button # 0, Exit");
+
+
+
+                var choices = (Console.ReadLine());
+
+                switch (choices)
+                {
+                    case "1":
+                        Console.WriteLine("Show all AUTO");
+                        carservice.WithOutSortPrice();
+                        break;
+                    case "2":
+                        Console.WriteLine("Cost of all cars");
+                        carservice.PriceCar();
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Sort Fuel By Ascending");
+                        carservice.SortFuelByAscending();
+                        break;
+                    case "4":
+                        Console.WriteLine("Sort Fuel By Descending");
+
+                        carservice.SortFuelByDescending();
+                        break;
+                    case "5":
+                        Console.WriteLine("Sort Price By Ascending");
+                        carservice.SortPriceByAscending();
+                        break;
+                    case "6":
+                        Console.WriteLine("Sort Price By Descending");
+                        carservice.SortPriceByDescending();
+                        break;
+                    case "7":
+                        Console.WriteLine("Search Cars Speed");
+                        carservice.SearchCarsSpeed(UserInputNumber(), UserInputNumber());
+                        break;
+
+                    case "0":
+                        exit = true;
+                        break;
+                }
+
+            }
+            while (!exit);
 
 
 
@@ -36,47 +95,22 @@ namespace TaxiDep
 
 
 
+            static int UserInputNumber()
+            {
+                int staded;
+                Console.Write("Input number: ");
+                var checkStaded = Console.ReadLine();
+                bool isSuccessful = int.TryParse(checkStaded, out staded);
+                while (!isSuccessful)
+                {
+                    Console.WriteLine("Error");
+                    Console.Write("Input number: ");
+                    checkStaded = Console.ReadLine();
+                    isSuccessful = int.TryParse(checkStaded, out staded);
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-            //  carservice.WithOutSortPrice();
-          //  carservice.SortFuelByAscending();
-           // carservice.SortFuelByDescending();
-
-
-           //carservice.SortPriceByAscending();
-          // carservice.SortPriceByDescending();
-
-          
-
-           //++  carservice.SortPriceByDescending();
-
-           //++  carservice.PriceCar();
-
-       
-         //++    carservice.SortFuelByDescending();
-
-
-          // carservice.SearchCarsSpeed( 100, 3000);
-
-           
-
-
-
-
-
-
-
+                return staded;
+            }
 
 
 
@@ -84,10 +118,6 @@ namespace TaxiDep
 
 
         }
-
-
-
-
     }
 }
 
